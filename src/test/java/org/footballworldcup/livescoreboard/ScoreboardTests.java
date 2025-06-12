2bb59103ba061bb3cbd485d56207f92231320434
Gabriela Ossowska <gab.ossowska@gmail.com>
2025-06-12 23:33:46 +0200

    expand test for ordering by score to include both running and finished matches

--- src/test/java/org/footballworldcup/livescoreboard/ScoreboardTests.java
+++ src/test/java/org/footballworldcup/livescoreboard/ScoreboardTests.java
@@ -98,30 +98,57 @@ public class ScoreboardTests {
     }
 
     @Test
-    public void givenTwoRunningMatchesWithDifferentScores_summaryShouldHaveThemOrdered()
+    public void givenMatchesWithDifferentScores_summaryShouldHaveThemOrdered()
             throws ClashingTeamsException, BlankTeamNameException, LowerScoreException, MatchNotFoundException {
         Scoreboard scoreboard = new Scoreboard();
+
+        // set-up:
+        // running: match1: total score 0, match2: total score 3
+        // finished: match3: total score 1, match4: total score 2
+        // expected order: match2, match4, match3, match1
+
+        // match 1
         String homeTeam = "Home";
         String awayTeam = "Away";
         scoreboard.start(homeTeam, awayTeam);
 
-        // start another match
+        // match 2
         String homeTeam2 = "Home2";
         String awayTeam2 = "Away2";
+        int newHomeTeam2Score = 1;
+        int newAwayTeam2Score = 2;
         scoreboard.start(homeTeam2, awayTeam2);
+        scoreboard.update(homeTeam2, awayTeam2, newHomeTeam2Score, newAwayTeam2Score);
 
-        // update the scores
-        int newHomeTeamScore = 1;
-        int newAwayTeamScore = 0;
-        int newHome2TeamScore = 1;
-        int newAway2TeamScore = 1;
-        scoreboard.update(homeTeam, awayTeam, newHomeTeamScore, newAwayTeamScore);
-        scoreboard.update(homeTeam2, awayTeam2, newHome2TeamScore, newAway2TeamScore);
+        // match 3
+        String homeTeam3 = "Home3";
+        String awayTeam3 = "Away3";
+        int newHomeTeam3Score = 0;
+        int newAwayTeam3Score = 1;
+        scoreboard.start(homeTeam3, awayTeam3);
+        scoreboard.update(homeTeam3, awayTeam3, newHomeTeam3Score, newAwayTeam3Score);
+        scoreboard.finish(homeTeam3, awayTeam3);
+
+        // match 4
+        String homeTeam4 = "Home4";
+        String awayTeam4 = "Away4";
+        int newHome4TeamScore = 2;
+        int newAway4TeamScore = 0;
+        scoreboard.start(homeTeam4, awayTeam4);
+        scoreboard.update(homeTeam4, awayTeam4, newHome4TeamScore, newAway4TeamScore);
+        scoreboard.finish(homeTeam4, awayTeam4);
 
         // check order
         List<Match> matches = scoreboard.getSummary();
         Assert.assertEquals(homeTeam2, matches.getFirst().getHomeTeam());
         Assert.assertEquals(awayTeam2, matches.getFirst().getAwayTeam());
+
+        Assert.assertEquals(homeTeam4, matches.get(1).getHomeTeam());
+        Assert.assertEquals(awayTeam4, matches.get(1).getAwayTeam());
+
+        Assert.assertEquals(homeTeam3, matches.get(2).getHomeTeam());
+        Assert.assertEquals(awayTeam3, matches.get(2).getAwayTeam());
+
         Assert.assertEquals(homeTeam, matches.getLast().getHomeTeam());
         Assert.assertEquals(awayTeam, matches.getLast().getAwayTeam());
     }
