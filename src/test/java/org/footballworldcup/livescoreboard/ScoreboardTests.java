3e9b2493bb14bec7c2d9ddf4f56c9b529edfacb8
Gabriela Ossowska <gab.ossowska@gmail.com>
2025-06-13 01:17:16 +0200

    add a test for summary method with 5 matches

--- src/test/java/org/footballworldcup/livescoreboard/ScoreboardTests.java
+++ src/test/java/org/footballworldcup/livescoreboard/ScoreboardTests.java
@@ -210,4 +210,80 @@ public class ScoreboardTests {
         Assert.assertTrue(scoreboard.getSummary().isEmpty());
     }
 
+    @Test
+    public void givenMatches_summaryShouldHaveThemOrdered() {
+        Scoreboard scoreboard = new Scoreboard();
+
+        // set-up (running unless otherwise stated):
+        // Mexico - Canada: 0 – 5 - finished
+        // Spain - Brazil: 10 – 2 - finished
+        // Germany - France: 2 – 2
+        // Uruguay - Italy: 6 – 6
+        // Argentina - Australia: 3 - 1
+
+        String mexico = "Mexico";
+        String canada = "Canada";
+        int mexicoScore = 0;
+        int canadaScore = 5;
+        startUpdateAndFinish(scoreboard, mexico, canada, mexicoScore, canadaScore);
+
+        String spain = "Spain";
+        String brazil = "Brazil";
+        int spainScore = 10;
+        int brazilScore = 2;
+        startUpdateAndFinish(scoreboard, spain, brazil, spainScore, brazilScore);
+
+        String germany = "Germany";
+        String france = "France";
+        int germanyScore = 2;
+        int franceScore = 2;
+        startAndUpdate(scoreboard, germany, france, germanyScore, franceScore);
+
+        String uruguay = "Uruguay";
+        String italy = "Italy";
+        int uruguayScore = 6;
+        int italyScore = 6;
+        startAndUpdate(scoreboard, uruguay, italy, uruguayScore, italyScore);
+
+        String argentina = "Argentina";
+        String australia = "Australia";
+        int argentinaScore = 3;
+        int australiaScore = 1;
+        startAndUpdate(scoreboard, argentina, australia, argentinaScore, australiaScore);
+
+        // expected result:
+        // Uruguay 6 - Italy 6
+        // Spain 10 - Brazil 2
+        // Mexico 0 - Canada 5
+        // Argentina 3 - Australia 1
+        // Germany 2 - France 2
+
+        List<SummarizedMatch> matches = scoreboard.getSummary();
+        assertMatchAsExpected(matches.get(0), uruguay, italy, uruguayScore, italyScore);
+        assertMatchAsExpected(matches.get(1), spain, brazil, brazilScore, spainScore);
+        assertMatchAsExpected(matches.get(2), mexico, canada, mexicoScore, canadaScore);
+        assertMatchAsExpected(matches.get(3), argentina, australia, argentinaScore, australiaScore);
+        assertMatchAsExpected(matches.get(4), germany, france, germanyScore, franceScore);
+    }
+
+    private static void assertMatchAsExpected(SummarizedMatch match, String homeTeam, String awayTeam,
+                                              int homeTeamScore, int awayTeamScore) {
+        Assert.assertEquals(homeTeam, match.getHomeTeam());
+        Assert.assertEquals(awayTeam, match.getAwayTeam());
+        Assert.assertEquals(homeTeamScore, match.getHomeTeamScore());
+        Assert.assertEquals(awayTeamScore, match.getAwayTeamScore());
+    }
+
+    private static void startAndUpdate(Scoreboard scoreboard, String homeTeam, String awayTeam,
+                                       int homeTeamScore, int awayTeamScore) {
+        scoreboard.start(homeTeam, awayTeam);
+        scoreboard.update(homeTeam, awayTeam, homeTeamScore, awayTeamScore);
+    }
+
+    private static void startUpdateAndFinish(Scoreboard scoreboard, String homeTeam, String awayTeam,
+                                             int homeTeamScore, int awayTeamScore) {
+        startAndUpdate(scoreboard, homeTeam, awayTeam, homeTeamScore, awayTeamScore);
+        scoreboard.finish(homeTeam, awayTeam);
+    }
+
 }
