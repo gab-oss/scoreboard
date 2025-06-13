ed9a2c204e5a8d74a01fb16d2eeb9d4563765793
Gabriela Ossowska <gab.ossowska@gmail.com>
2025-06-13 03:24:52 +0200

    fill in toString in SummarizedMatch to return formatted result

--- src/main/java/org/footballworldcup/livescoreboard/SummarizedMatch.java
+++ src/main/java/org/footballworldcup/livescoreboard/SummarizedMatch.java
@@ -3,6 +3,6 @@ package org.footballworldcup.livescoreboard;
 public record SummarizedMatch(String homeTeam, String awayTeam, int homeTeamScore, int awayTeamScore) {
     @Override
     public String toString() {
-        return "";
+        return String.format("%s %d - %s %d", homeTeam, homeTeamScore, awayTeam, awayTeamScore());
     }
 }
